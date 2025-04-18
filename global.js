672e9ca041e8e8d56fa6a393a8f82327c2162773
msharma-26 <mksharma@ucsd.edu>
2025-04-18 14:09:09 -0700

    updated the for loop for nav menu

--- global.js
+++ global.js
@@ -15,33 +15,34 @@ console.log('IT’S ALIVE!');
 // }
 
 let pages = [
-    { url: '', title: 'Home' },
-    { url: 'projects/', title: 'Projects' },
-    {url: 'contact/', title: "Contact"},
-    {url: 'resume/', title: 'Resume'}
-  ];
+  { url: '', title: 'Home' },
+  { url: 'projects/', title: 'Projects' },
+  { url: 'contact/', title: 'Contact' },
+  { url: 'resume/', title: 'Resume' }
+];
 
 let nav = document.createElement('nav');
 document.body.prepend(nav);
 
-for (let p of pages) {
-    let url = p.url;
-    let title = p.title;
-    // next step: create link and add it to nav
-    let a = document.createElement('a');
-    a.href = url;
-    a.textContent = title;
-    nav.append(a);
-}
-
 const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
   ? "/"                  // Local server
-  : "/website/"; 
+  : "/website/";         // Production or GitHub Pages-like path
+
+for (let p of pages) {
+  let url = p.url;
+  let title = p.title;
 
   if (!url.startsWith('http')) {
     url = BASE_PATH + url;
   }
 
+  let a = document.createElement('a');
+  a.href = url;
+  a.textContent = title;
+
   if (a.host === location.host && a.pathname === location.pathname) {
     a.classList.add('current');
   }
+
+  nav.append(a);
+}
