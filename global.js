6821b18dc8760723fccd2fcb54161cd87b48f176
msharma-26 <mksharma@ucsd.edu>
2025-04-18 00:19:03 -0700

    created automatic navigation menu

--- global.js
+++ global.js
@@ -1,15 +1,40 @@
 console.log('IT’S ALIVE!');
 
-function $$(selector, context = document) {
-  return Array.from(context.querySelectorAll(selector));
-}
-navLinks = $$("nav a")
+// function $$(selector, context = document) {
+//   return Array.from(context.querySelectorAll(selector));
+// }
+// navLinks = $$("nav a")
 
-let currentLink = navLinks.find(
-    (a) => a.host === location.host && a.pathname === location.pathname,
-  );
+// let currentLink = navLinks.find(
+//     (a) => a.host === location.host && a.pathname === location.pathname,
+//   );
 
-if (currentLink) {
-    // or if (currentLink !== undefined)
-    currentLink.classList.add('current');
+// if (currentLink) {
+//     // or if (currentLink !== undefined)
+//     currentLink.classList.add('current');
+// }
+
+let pages = [
+    { url: '', title: 'Home' },
+    { url: 'projects/', title: 'Projects' },
+    {url: 'contact/', title: "Contact"}
+    {url: 'resume/', title: 'Resume'}
+  ];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+    // next step: create link and add it to nav
+    nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
 }
+
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+  ? "/"                  // Local server
+  : "/website/"; 
+
+  if (!url.startsWith('http')) {
+    url = BASE_PATH + url;
+  }
